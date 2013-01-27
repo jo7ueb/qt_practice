e12947f5d1c3b1d1827974893664a6cec10dcc91
Yasunori Endo <a1202006@sendai-nct.jp>
2013-01-27 18:17:46 +0900

    add some test codes

--- dwaring_on_qimage/MainWindow.cpp
+++ dwaring_on_qimage/MainWindow.cpp
@@ -23,6 +23,18 @@ MainWindow::MainWindow(QWidget *parent) :
     QPixmap pmap = QPixmap::fromImage(image);
     ui->label->setPixmap(pmap);
 
+    // try to paint text more
+    painter.drawText(100, 100, "Another text");
+    /* This text can't be seen from GUI now.
+     * But image file is changed.
+     */
+
+#if 0
+    // try to repaint window widget -- don't work!
+    this->repaint(0, 0, 150, 150);
+    painter.drawText(110, 110, "Nothing will happen!");
+#endif
+
     // save image file
     image.save("test_image.png");
 }
